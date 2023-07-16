8a836bfd335d3f7afa07fe9d93d23e1bfb9848f3
Hankypon <131440220+hankypon@users.noreply.github.com>
2023-07-16 12:56:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+
+
+const http = require('http');
+ 
+const server = http.createServer(function (req, res) {
+ switch(true)
+ {
+    case req.url === '/' && req.method === 'GET':
+    res.setHeader('content-type', 'text/html; charset=utf-8');
+    res.writeHead(200);
+    res.end('Címlap <a href="/bejelentkezes">Bejelentkezés</a>');
+    break;
+    case req.url === '/bejelentkezes' && req.method === 'GET':
+        res.setHeader('content-type', 'text/html; charset=utf-8');
+        res.writeHead(200);
+        res.end('Bejelentkezés <a href="/">Vissza a főoldalra</a>');
+        break;
+    default:
+    res.setHeader('content-type', 'text/html; charset=utf-8');
+    res.writeHead(400);
+    res.end('Oldal nem található  <a href="/">Vissza a főoldalra</a>');
+ }
+    console.log(req.url);
+    console.log(req.method);
+    
+  });
+server.listen(8080);
